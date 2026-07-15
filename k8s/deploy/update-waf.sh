b1d035bbfcdc043b9fc587192090f2d4b2b16816
L.JangHyun <zxcz9878@gmail.com>
2026-07-15 23:30:49 +0900

    chore(#61): 파일 위치 변경

--- k8s/deploy/update-waf.sh
+++ k8s/deploy/update-waf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 # Terraform 결과 가져오기
 echo "🔍 Terraform에서 WAF ARN을 가져오는 중..."
-WAF_ARN="$(cd "$SCRIPT_DIR/terraform/environments/dev" && terraform output -raw waf_acl_arn)"
+WAF_ARN="$(cd "$BASE_DIR/terraform/environments/dev" && terraform output -raw waf_acl_arn)"
 
 if ! echo "$WAF_ARN" | grep -Eq "^arn:aws:wafv2"; then
     echo "❌ 실패: 값이 일치하지 않거나 비어 있습니다."
@@ -15,7 +15,7 @@ fi
 echo "✅ 성공: $WAF_ARN"
 
 # values 파일 수정
-cd "${SCRIPT_DIR}/k8s/spot-apps"
+cd "$BASE_DIR/k8s/spot-apps"
 echo "📝 values-dev.yaml 파일을 수정하는 중..."
 
 WAF_ARN=$WAF_ARN yq -i '.ingress.annotations."alb.ingress.kubernetes.io/wafv2-acl-arn" = env(WAF_ARN)' values-dev.yaml
