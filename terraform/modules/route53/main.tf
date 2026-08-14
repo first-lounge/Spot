6ab4d3e15309860267611c5f857e2b4db9bfc3a5
L.JangHyun <zxcz9878@gmail.com>
2026-08-14 22:24:23 +0900

    fix(#77): route53 zone에 force_destroy 변수 추가

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -2,6 +2,7 @@
 # Route53 Hosted Zone
 # =============================================================================
 resource "aws_route53_zone" "main" {
-  name = var.domain_name
-  tags = merge(var.common_tags, { Name = "${var.name_prefix}-zone" })
+  name          = var.domain_name
+  force_destroy = var.zone_force_destroy
+  tags          = merge(var.common_tags, { Name = "${var.name_prefix}-zone" })
 }
